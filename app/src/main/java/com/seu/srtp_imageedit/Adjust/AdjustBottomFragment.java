2098df5cf2209807a13dd5a05b927fcfdd550bf8
zhoubin <15850652918@163.com>
2016-08-10 20:28:55 +0800

    完成图像调整功能下进度条状态的暂存，实现调整透明度的效果

--- app/src/main/java/com/seu/srtp_imageedit/Adjust/AdjustBottomFragment.java
+++ app/src/main/java/com/seu/srtp_imageedit/Adjust/AdjustBottomFragment.java
@@ -2,6 +2,7 @@ package com.seu.srtp_imageedit.Adjust;
 
 import android.graphics.Bitmap;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,11 +12,13 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.Gallery;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.PopupWindow;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import com.seu.srtp_imageedit.Frame.FrameUtil;
@@ -25,21 +28,37 @@ import com.seu.srtp_imageedit.ImageGalleryAdapter;
 import com.seu.srtp_imageedit.ImageTextGalleryAdapter;
 import com.seu.srtp_imageedit.R;
 
+import java.nio.MappedByteBuffer;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2016/8/2.
  */
-public class AdjustBottomFragment extends Fragment{
+public class AdjustBottomFragment extends Fragment implements SeekBar.OnSeekBarChangeListener{
+
+    private int ADJUST_STATUS;//保存当前调整的属性
+    private String seekBarStatus;//保存当前进度条状态
+    private String seekBarVisible="VISIBLE";//进度条可见
+    private String seekBarGone="GONE";//进度条被隐藏
+    private int MAX_VALUE=255;//进度条的最大值
+    private int MID_VALUE=127;//进度条的中间值
 
     private ImageButton mChooseAdjustButton;
+    private Button mValueButton;
+    private Button mFunctionButton;
     private ImageView mImageView;//该碎片所在的活动的ImageView
+    private Drawable srcBitmapDrawable;//待处理图像的内容
     private Bitmap mBitmap;//处理后的图片
 
     private List<Function_Item> mAdjustFunctionList=new ArrayList<Function_Item>();//图片调整功能列表
     private ListView mListView;
     private Function_Item_Adapter adapter;
+    private SeekBar mSeekBar;
+
+    private Map<Integer,Integer> mAdjustValueMap=new HashMap<>();
 
     private float mLuminance;//亮度
     private float mHue;//色调
@@ -47,6 +66,15 @@ public class AdjustBottomFragment extends Fragment{
     private float mContrast;//对比度
     private float mTransparency;//透明度
     private float mTemperature;//色温
+
+    private void initMap(){
+        mAdjustValueMap.put(R.string.adjust_luminance,MID_VALUE);
+        mAdjustValueMap.put(R.string.adjust_hue,MID_VALUE);
+        mAdjustValueMap.put(R.string.adjust_saturation,MID_VALUE);
+        mAdjustValueMap.put(R.string.adjust_contrast,MID_VALUE);
+        mAdjustValueMap.put(R.string.adjust_transparency,MID_VALUE);
+        mAdjustValueMap.put(R.string.adjust_temperature,MID_VALUE);
+    }
 
     private void initFunctionList(){
         Function_Item luminance_item=new Function_Item(R.string.adjust_luminance,R.drawable.ic_luminance_button);
@@ -68,18 +96,47 @@ public class AdjustBottomFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.bottom_fragment_adjust,container,false);
 
-         initFunctionList();
-         mImageView= (ImageView) getActivity().findViewById(R.id.image_to_edit);
-         mChooseAdjustButton=(ImageButton)v.findViewById(R.id.choose_adjust_effect_button);
-         mChooseAdjustButton.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 //Toast.makeText(getActivity(), "choose adjust effect", Toast.LENGTH_SHORT).show();
-                 showPopUpWindow(v);//显示悬浮框选择功能
-             }
-         });
-
+        initFunctionList();
+        initMap();
+        mSeekBar=(SeekBar)getActivity().findViewById(R.id.adjust_seekbar);
+        mSeekBar.setMax(MAX_VALUE);//设置最大值
+        mSeekBar.setProgress(MID_VALUE);//设置初始的进度
+        mSeekBar.setOnSeekBarChangeListener(this);
+        mImageView= (ImageView) getActivity().findViewById(R.id.image_to_edit);
+        srcBitmapDrawable=mImageView.getDrawable();
+        mChooseAdjustButton=(ImageButton)v.findViewById(R.id.choose_adjust_effect_button);
+        mChooseAdjustButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getActivity(), "choose adjust effect", Toast.LENGTH_SHORT).show();
+                //隐藏进度条
+                mSeekBar.setVisibility(View.GONE);
+                showPopUpWindow(v);//显示悬浮框选择功能
+            }
+        });
+        mValueButton=(Button)v.findViewById(R.id.show_adjust_seekbar_value);
+        mFunctionButton=(Button)v.findViewById(R.id.show_adjust_function);
         return v;
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        mValueButton.setText(""+progress);
+        //保存当前设置的值
+        mAdjustValueMap.put(ADJUST_STATUS,progress);
+        mBitmap=AdjustUtil.AdjustImage(ADJUST_STATUS,progress,srcBitmapDrawable);
+        mImageView.setImageBitmap(mBitmap);
+        //mImageView.setImageAlpha(progress/100);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 
     /**
@@ -92,14 +149,6 @@ public class AdjustBottomFragment extends Fragment{
         adapter=new Function_Item_Adapter(getActivity(),R.layout.operation_list_item,mAdjustFunctionList);
         mListView.setAdapter(adapter);
 
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Function_Item item = mAdjustFunctionList.get(position);
-                Toast.makeText(getActivity(), item.getmFuntionNameId(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
         final PopupWindow popupWindow=new PopupWindow(view,mImageView.getWidth()/2,500,true);
         popupWindow.setTouchable(true);
         popupWindow.setTouchInterceptor(new View.OnTouchListener() {
@@ -111,9 +160,29 @@ public class AdjustBottomFragment extends Fragment{
         //必须设置背景，否则悬浮框不会消失
         popupWindow.setBackgroundDrawable(new ColorDrawable(0x00000000));
         //悬浮框出现在图片区域中间
-        popupWindow.showAtLocation(mImageView, Gravity.CENTER,0,0);
+        popupWindow.showAtLocation(mImageView, Gravity.CENTER, 0, 0);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //点击选择的属性后，悬浮框消失
+                if (popupWindow.isShowing()){
+                    popupWindow.dismiss();
+                }
+                Function_Item item = mAdjustFunctionList.get(position);
+                //Toast.makeText(getActivity(), item.getmFuntionNameId(), Toast.LENGTH_SHORT).show();
+                //当前正在调整的图片属性
+                ADJUST_STATUS = item.getmFuntionNameId();
+                //显示进度条or使用手势操作
+                mSeekBar.setVisibility(View.VISIBLE);
+                //显示当前选择的功能和调整的值
+                mFunctionButton.setText(ADJUST_STATUS);
+                mValueButton.setText(""+mAdjustValueMap.get(ADJUST_STATUS));
+                //将进度条调整至此功能下设置的值
+                mSeekBar.setProgress(mAdjustValueMap.get(ADJUST_STATUS));
+            }
+        });
+
+
     }
-
-
-
 }
--- app/src/main/java/com/seu/srtp_imageedit/Adjust/AdjustUtil.java
+++ app/src/main/java/com/seu/srtp_imageedit/Adjust/AdjustUtil.java
@@ -1,8 +1,56 @@
 package com.seu.srtp_imageedit.Adjust;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+
+import com.seu.srtp_imageedit.Image;
+import com.seu.srtp_imageedit.R;
+
 /**
  * Created by Administrator on 2016/8/9.
  */
 public class AdjustUtil {
+    private static Matrix mMatrix=new Matrix();
+    private static ColorMatrix mColorMatrix=new ColorMatrix();
+    private static Paint mPaint=new Paint(Paint.ANTI_ALIAS_FLAG);
+    private static Canvas mCanvas;
 
+    public static Bitmap AdjustImage(int adjust_status,int progress,Drawable srcBitmapDrawable){
+        Bitmap bm=null;
+        switch (adjust_status){
+            case R.string.adjust_transparency:
+                bm=transparency(srcBitmapDrawable,progress);
+                break;
+            default:
+                break;
+        }
+        return bm;
+    }
+
+    /**
+     * 调整透明度
+     * @param srcBitmapDrawable
+     * @param progress
+     * @return
+     */
+    private static Bitmap transparency(Drawable srcBitmapDrawable,int progress){
+        Bitmap srcBitmap= Image.DrawableToBitmap(srcBitmapDrawable);
+        Bitmap destBitmap=Bitmap.createBitmap(srcBitmap.getWidth(),srcBitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        mCanvas=new Canvas(destBitmap);
+
+        mColorMatrix.set(new float[]{
+                1,0,0,0,0,
+                0,1,0,0,0,
+                0,0,1,0,0,
+                0,0,0,progress/255.0f,0,
+        });
+        mPaint.setColorFilter(new ColorMatrixColorFilter(mColorMatrix));
+        mCanvas.drawBitmap(srcBitmap,0,0,mPaint);
+        return destBitmap;
+    }
 }
